486b2557be458989145efb8a8ca34d5349bad875
er-roshan <roshan.adhikari.se@gmail.com>
2022-10-22 20:17:51 +0545

    Create: New Customer adding Screen

--- pages/customers/create.js
+++ pages/customers/create.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
+import CustomerForm from '../../components/pages/customer/CustomerForm';
 
 const CustomerCreate = () => {
-  return (
-    <div>CustomerCreate</div>
-  )
-}
+	return (
+		<div className='container'>
+			<div className='flex justify-between items-center mb-4'>
+				<h2 class='text-2xl font-medium leading-6 text-gray-900 '>
+					New Customer
+				</h2>
+				<Link href={'/customers/create'}>
+					<a class='default-btn'>Go Back</a>
+				</Link>
+			</div>
+      <CustomerForm />
+		</div>
+	);
+};
 
-export default CustomerCreate
+export default CustomerCreate;
